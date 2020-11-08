2935efa388b7bf2afa355b0d07a28ca657aeedb3
Krishnil Bhojani <krishnilbhojani2001@gmail.com>
2020-11-08 13:25:24 +0530

    Initial Commit

---
+++ FancyCharts/FancyCharts.h
@@ -0,0 +1,18 @@
+//
+//  FancyCharts.h
+//  FancyCharts
+//
+//  Created by Qtis Developer on 08/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FancyCharts.
+FOUNDATION_EXPORT double FancyChartsVersionNumber;
+
+//! Project version string for FancyCharts.
+FOUNDATION_EXPORT const unsigned char FancyChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FancyCharts/PublicHeader.h>
+
+
